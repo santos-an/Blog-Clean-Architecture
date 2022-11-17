8c87b980651b0acf5ad8d86f1804594807488e52
santos-an <6472330+santos-an@users.noreply.github.com>
2022-11-17 17:50:05 +0000

    add domain tests

---
+++ test/Domain.Tests/CommentTests.cs
@@ -0,0 +1,87 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.Tests;
+
+public class CommentTests
+{
+    private readonly Comment _comment;
+
+    public CommentTests()
+    {
+        _comment = new Comment()
+        {
+            Id = Guid.NewGuid(),
+            Author = "new author",
+            Content = "new content",
+            PostId = Guid.NewGuid(),
+            CreationDate = DateTime.Now
+        };
+    }
+
+    [Fact]
+    public void SetAndGetId_UpdatesEntity()
+    {
+        // arrange
+        var expected = Guid.NewGuid();
+        
+        // act
+        _comment.Id = expected;
+
+        // assert
+        expected.Should().Be(_comment.Id);
+    }
+    
+    [Fact]
+    public void SetAndGetAuthor_UpdatesEntity()
+    {
+        // arrange
+        var expected = "new author";
+        
+        // act
+        _comment.Author = expected;
+
+        // assert
+        expected.Should().Be(_comment.Author);
+    }
+    
+    [Fact]
+    public void SetAndGetContent_UpdatesEntity()
+    {
+        // arrange
+        var expected = "new content";
+        
+        // act
+        _comment.Content = expected;
+
+        // assert
+        expected.Should().Be(_comment.Content);
+    }
+    
+    [Fact]
+    public void SetAndGetPostId_UpdatesEntity()
+    {
+        // arrange
+        var expected = Guid.NewGuid();
+        
+        // act
+        _comment.PostId = expected;
+
+        // assert
+        expected.Should().Be(_comment.PostId);
+    }
+    
+    [Fact]
+    public void SetAndGetCreationDate_UpdatesEntity()
+    {
+        // arrange
+        var expected = DateTime.Now;
+        
+        // act
+        _comment.CreationDate = expected;
+
+        // assert
+        expected.Should().Be(_comment.CreationDate);
+    }
+}
---
+++ test/Domain.Tests/PostTests.cs
@@ -0,0 +1,107 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.Tests;
+
+public class PostTests
+{
+    private readonly Post _post;
+    
+    public PostTests()
+    {
+        _post = new Post()
+        {
+            Id = Guid.NewGuid(),
+            Content = string.Empty,
+            Title = string.Empty,
+            CreationDate = DateTime.Now,
+            Comments = new List<Comment>()
+            {
+                new ()
+                {
+                    Id = Guid.NewGuid(),
+                    PostId = Guid.NewGuid(),
+                    Author = "author 1",
+                    Content = "content 1",
+                    CreationDate = DateTime.Now
+                }
+            }
+        };
+    }
+
+    [Fact]
+    public void SetAndGetId_UpdatesEntity()
+    {
+        // arrange
+        var expected = Guid.NewGuid();
+        
+        // act
+        _post.Id = expected;
+
+        // assert
+        expected.Should().Be(_post.Id);
+    }
+
+    [Fact]
+    public void SetAndGetTitle_UpdatesEntity()
+    {
+        // arrange
+        var expected = "new title";
+        
+        // act
+        _post.Title = expected;
+
+        // assert
+        expected.Should().Be(_post.Title);
+    }
+
+    [Fact]
+    public void SetAndGetContent_UpdatesEntity()
+    {
+        // arrange
+        var expected = "new content";
+        
+        // act
+        _post.Content = expected;
+
+        // assert
+        expected.Should().Be(_post.Content);
+    }
+    
+    [Fact]
+    public void SetAndGetCreationDate_UpdatesEntity()
+    {
+        // arrange
+        var expected = DateTime.Today;
+        
+        // act
+        _post.CreationDate = expected;
+
+        // assert
+        expected.Should().Be(_post.CreationDate);
+    }
+    
+    [Fact]
+    public void SetAndGetComments_UpdatesEntity()
+    {
+        // arrange
+        var expected = new List<Comment>()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Author = "new author 1",
+                Content = "about this interview",
+                CreationDate = DateTime.Today,
+                PostId = Guid.NewGuid()
+            }
+        };
+        
+        // act
+        _post.Comments = expected;
+
+        // assert
+        expected.Should().NotBeEmpty();
+    }
+}
